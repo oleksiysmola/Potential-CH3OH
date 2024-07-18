84d79e5eca8aa986866c13c108ef614b83ad6a32
Oleksiy <oleksiy.smola@gmail.com>
2024-07-18 16:24:03 +0100

    defined symmetry operations

--- CH3OH.jl
+++ CH3OH.jl
@@ -1,4 +1,4 @@
-function generateSymmetryOperations(case="C3v(M)"::String)::Array{Float64}
+function defineSymmetryOperations(case="C3v(M)"::String)::Array{Float64}
     if case == "C3v(M)"
         symmetryOperations::Array{Float64} = zeros(Int64, 6, 11, 11)
         symmetryOperations[1, :, :] = Matrix(1I, 11, 11)
@@ -76,15 +76,40 @@ function generateSymmetryOperations(case="C3v(M)"::String)::Array{Float64}
     return symmetryOperations
 end
 
-function generateSymmetryOperationsCartesianRepresentation(zMatrix::DataFrame, case="C3v(M)"::String)
+function defineTorsionSymmetryOperations(fourierOrder::Int64, case="C3v(M)"::String)::Array{Float64}
     if case == "C3v(M)"
-        cartesianSymmetryOperations = zeros(Int64, 6, 18, 18)
-        cartesianSymmetryOperations[1, :, :] = matrix(1I, 18, 18)
+        torsionSymmetryOperations::Array{Float64} = zeros(Int64, 6, 2, 2)
+        torsionSymmetryOperations[1, :, :] = Matrix(1I, 2, 2)
+        # (123)
+        torsionSymmetryOperations[2, :, :] = [
+            cos(2*pi*fourierOrder/3) -sin(2*pi*fourierOrder/3);
+            sin(2*pi*fourierOrder/3) cos(2*pi*fourierOrder/3);           
+            ]
+        # (132)
+        torsionSymmetryOperations[3, :, :] = [
+            cos(2*pi*fourierOrder/3) sin(2*pi*fourierOrder/3);
+            -sin(2*pi*fourierOrder/3) cos(2*pi*fourierOrder/3);           
+            ]
+        # (12)*
+        torsionSymmetryOperations[4, :, :] = [
+            cos(2*pi*fourierOrder/3) -sin(2*pi*fourierOrder/3);
+            -sin(2*pi*fourierOrder/3) -cos(2*pi*fourierOrder/3);           
+            ]
+        # (23)*
+        torsionSymmetryOperations[5, :, :] = [
+            1 0;
+            0 -1;           
+            ]
+        # (13)*
+        torsionSymmetryOperations[6, :, :] = [
+            cos(2*pi*fourierOrder/3) sin(2*pi*fourierOrder/3);
+            sin(2*pi*fourierOrder/3) -cos(2*pi*fourierOrder/3);           
+            ]
     end
-    return cartesianSymmetryOperations
+    return symmetryOperations
 end
 
-function generateXiCoordinates(localModeCoordinates::Vector{SymPy.Sym})::Vector{SymPy.Sym}
+function generateXiCoordinates(localModeCoordinates::Vector{Float64})::Vector{Float64}
     convertToRadians::Float64 = 2*pi/360
     # Define equilibrium parameters
     rCOeq::Float64 = 1.4296
@@ -96,25 +121,25 @@ function generateXiCoordinates(localModeCoordinates::Vector{SymPy.Sym})::Vector{
     a2::Float64 = 1.66
     b1::Float64 = 1.55
     
-    xi::Vector{SymPy.Sym} = Vector{SymPy.Sym}()
+    xi::Vector{Float64} = zeros(Float64, 13)
     
     # Stretches
-    push!(xi, 1.00 - exp(-a1*(localModeCoordinates[1] - rCOeq)))
-    push!(xi, 1.00 - exp(-a2*(localModeCoordinates[2] - rOHeq)))
-    push!(xi, 1.00 - exp(-b1*(localModeCoordinates[3] - rH1eq)))
-    push!(xi, 1.00 - exp(-b1*(localModeCoordinates[4] - rH1eq)))
-    push!(xi, 1.00 - exp(-b1*(localModeCoordinates[5] - rH1eq)))
+    xi[1] = 1.00 - exp(-a1*(localModeCoordinates[1] - rCOeq))
+    xi[2] = 1.00 - exp(-a2*(localModeCoordinates[2] - rOHeq))
+    xi[3] = 1.00 - exp(-b1*(localModeCoordinates[3] - rH1eq))
+    xi[4] = 1.00 - exp(-b1*(localModeCoordinates[4] - rH1eq))
+    xi[5] = 1.00 - exp(-b1*(localModeCoordinates[5] - rH1eq))
 
     # Angles
-    push!(xi, localModeCoordinates[6] - alphaCOHeq)
-    push!(xi, localModeCoordinates[7] - alphaOCHeq)
-    push!(xi, localModeCoordinates[8] - alphaOCHeq)
-    push!(xi, localModeCoordinates[9] - alphaOCHeq)
+    xi[6] = cos(localModeCoordinates[6]) - cos(alphaCOHeq)
+    xi[7] = cos(localModeCoordinates[7]) - cos(alphaOCHeq)
+    xi[8] = cos(localModeCoordinates[8]) - cos(alphaOCHeq)
+    xi[9] = cos(localModeCoordinates[9]) - cos(alphaOCHeq)
 
     # Define symmeterised dihedrals
-    d12::SymPy.Sym = localModeCoordinates[11] - localModeCoordinates[10]
-    d23::SymPy.Sym = localModeCoordinates[12] - localModeCoordinates[11]
-    d13::SymPy.Sym = localModeCoordinates[10] - localModeCoordinates[12]
+    d12::Float64 = localModeCoordinates[11] - localModeCoordinates[10]
+    d23::Float64 = localModeCoordinates[12] - localModeCoordinates[11]
+    d13::Float64 = localModeCoordinates[10] - localModeCoordinates[12]
     push!(xi, (2*d23 - d13 - d12)/sqrt(6))
     push!(xi, (d13 - d12)/sqrt(2))
     
